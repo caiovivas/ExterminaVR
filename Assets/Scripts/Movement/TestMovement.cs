5548a1f8183200968485f4cd8ad4c185fe4bdca4
Caio Vivas <caionvivas@yahoo.com.br>
2017-06-10 16:38:09 -0300

    Comeco do player e commit pra poder dar pull

---
+++ Assets/Scripts/Movement/TestMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour {
+	public CharacterController controller;
+	public float speed;
+	public float rotateSpeed;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		controller.SimpleMove (transform.forward* Input.GetAxis("Vertical") * Time.deltaTime * speed);
+		transform.RotateAround (transform.position, transform.up, Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerVision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVision : MonoBehaviour {
+
+	public Transform eye;
+	public LayerMask visionLayer;
+	public static PlayerVision vision;
+
+	void Start(){
+		vision = this;
+	}
+
+	public Transform GetObjectInPlayerSight(){
+		RaycastHit hit;
+		if (Physics.Raycast (eye.position, eye.forward, out hit, 100f, visionLayer)) {
+			return hit.transform;
+		}	
+	}
+}
