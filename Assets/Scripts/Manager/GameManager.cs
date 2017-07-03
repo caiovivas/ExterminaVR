695d83245ceb0939ed9f801f29f5f717eea05eec
Caio Vivas <caionvivas@yahoo.com.br>
2017-07-02 21:14:17 -0300

    Passarela

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -74,5 +74,6 @@ public class GameManager : MonoBehaviour {
 		if (playOnLoad != null)
 			PlaySound (playOnLoad);
 		playOnLoad = null;
+		macbonnerObject.GetComponent<Macbonner> ().Despawn ();
 	}
 }
